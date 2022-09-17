80aae3c5944351d8d988e3f7139f1409600bae63
William Denny <wdenny1999@gmail.com>
2022-09-17 19:33:56 +0100

    Seperated into forward and backward node

--- compGraph.hpp
+++ compGraph.hpp
@@ -5,6 +5,12 @@ Computational Graph library
 #include <vector>
 #include <assert.h>
 
+/*
+***************************************************************************************************************************
+NODE
+- contains value
+*/
+
 template <class T>
 class Node
 {
@@ -22,10 +28,63 @@ T Node<T>::value() const
 
 /*
 ***************************************************************************************************************************
+BACKWARD NODE
+- contains inputs
 */
 
 template <class T>
-class ForwardNode : public Node<T>
+class BackwardNode : virtual public Node<T>
+{
+protected:
+    int m_numInputs;
+    std::vector<Node<T>*> m_inputs;
+public:
+    virtual int findInput(const Node<T>* n) const;
+    virtual void addInput(Node<T>* n);
+    virtual void removeInput(Node<T>* n);
+};
+
+/* find  input index by node ptr, returns -1 if not found */
+template <class T>
+int BackwardNode<T>::findInput(const Node<T>* n) const
+{
+    for (int i = 0; i < this->m_numInputs; ++i)
+    {
+        if (this->m_inputs[i] == n)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/* add input by node ptr */
+template <class T>
+void BackwardNode<T>::addInput(Node<T>* n)
+{
+    assert(this->findInput(n) == -1); // make sure output isn't already present
+    this->m_inputs.push_back(n);
+    this->m_numInputs++;
+}
+
+/* remove input by node ptr */
+template <class T>
+void BackwardNode<T>::removeInput(Node<T>* n)
+{
+    int index = this->findInput(n);
+    assert(index != -1); // make sure input is present
+    this->m_inputs.erase(this->m_inputs.begin() + index);
+    this->m_numInputs--;
+}
+
+/*
+***************************************************************************************************************************
+FORWARD NODE
+- contains outputs
+*/
+
+template <class T>
+class ForwardNode : virtual public Node<T>
 {
 protected:
     int m_numOutputs;
@@ -36,6 +95,7 @@ public:
     virtual void removeOutput(Node<T>* n);
 };
 
+/* find output index by node ptr, returns -1 if not found */
 template <class T>
 int ForwardNode<T>::findOutput(const Node<T>* n) const
 {
@@ -49,6 +109,7 @@ int ForwardNode<T>::findOutput(const Node<T>* n) const
     return -1;
 }
 
+/* add output by node ptr */
 template <class T>
 void ForwardNode<T>::addOutput(Node<T>* n)
 {
@@ -57,6 +118,7 @@ void ForwardNode<T>::addOutput(Node<T>* n)
     this->m_numOutputs++;
 }
 
+/* remove output by node ptr */
 template <class T>
 void ForwardNode<T>::removeOutput(Node<T>* n)
 {
@@ -68,6 +130,10 @@ void ForwardNode<T>::removeOutput(Node<T>* n)
 
 /*
 ***************************************************************************************************************************
+INPUT
+- exec writes to value from argument
+- no inputs allowed
+- multiply outputs allowed
 */ 
 
 template <class T>
@@ -79,6 +145,7 @@ public:
     void exec(const T& value);
 };
 
+/* ctor */
 template <class T>
 Input<T>::Input()
 {
@@ -87,9 +154,93 @@ Input<T>::Input()
     this->m_value = 0.0;
 }
 
+/* execute */
 template <class T>
 void Input<T>::exec(const T& value)
 {
     this->m_value = value;
+}
+
+/*
+***************************************************************************************************************************
+OUTPUT
+- exec writes to value from single input value
+- only one input allowed
+- no outputs allowed
+*/ 
+
+template <class T>
+class Output : public BackwardNode<T>
+{
+private:
+public:
+    Output();
+    void addInput(Node<T>* n); // override add input to only allow 1 input
+    void exec();
+};
+
+/* ctor */
+template <class T>
+Output<T>::Output()
+{
+    this->m_numInputs = 0;
+    this->m_inputs = {};
+    this->m_value = 0.0;
+}
+
+/* execute */
+template <class T>
+void Output<T>::exec()
+{
+    this->m_value = this->m_inputs[0]->value();
+}
+
+/* add input override (only 1 input allowed) */
+template <class T>
+void Output<T>::addInput(Node<T>* n)
+{
+    assert(this->findInput(n) == -1); // make sure output isn't already present
+    assert(this->m_numInputs == 0); // only one input is allowed
+    
+    this->m_inputs.push_back(n);
+    this->m_numInputs++;
+}
+
+/*
+***************************************************************************************************************************
+ADD
+- exec sums input values into value
+- no limit on inputs/outputs
+*/ 
+
+template <class T>
+class Add : public BackwardNode<T>, public ForwardNode<T>
+{
+private:
+public:
+    Add();
+    void exec();
+};
+
+/* ctor */
+template <class T>
+Add<T>::Add()
+{
+    this->m_numInputs = 0;
+    this->m_numOutputs = 0;
+    this->m_inputs = {};
+    this->m_outputs = {};
+    this->m_value = 0.0;
+}
+
+/* execute */
+template <class T>
+void Add<T>::exec()
+{
+    this->m_value = 0.0; // set to identity
+    for (int i = 0; i < this->m_numInputs; ++i)
+    {
+        this->m_value += this->m_inputs[i]->value();
+    }
 }
 
